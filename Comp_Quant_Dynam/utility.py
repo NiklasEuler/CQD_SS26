54e1e164e8cd61c7c1b6edb086558eeeed0b9bbe
NiklasEuler <Niklas.Euler@t-online.de>
2026-04-07 12:21:58 +0200

    Update docstring and show help function.

--- Comp_Quant_Dynam/utility.py
+++ Comp_Quant_Dynam/utility.py
@@ -6,4 +6,5 @@ def example_func(x):
     Example function to demonstrate the repository structure.
     Returns the ground state wavefunction of the quantum harmonic oscillator at position 'x' in numerical units.
     """
+
     return 1 / np.pi ** (1 / 4) * np.exp(-x ** 2 / 2)
